fd1f6540620913fced29578cd51d7af8c086c6f0
DK Kim <dkim693@wisc.edu>
2024-01-06 17:49:57 -0600

    change middleware

--- middleware.js
+++ middleware.js
@@ -10,9 +10,9 @@ export async function middleware(req) {
   } = await supabase.auth.getUser()
 
   // if user is signed in and the current path is / redirect the user to /account
-  if (user && req.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/main', req.url))
-  }
+  // if (user && req.nextUrl.pathname === '/') {
+  //   return NextResponse.redirect(new URL('/main', req.url))
+  // }
 
   // if user is not signed in and the current path is not / redirect the user to /
   // if (!user && req.nextUrl.pathname !== '/') {
