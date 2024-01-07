41e59cb40c6c61268f8839fe21a1ff5f54cc1942
DK Kim <dkim693@wisc.edu>
2024-01-06 19:34:38 -0600

    google login fix

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,5 +1,6 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server'
 
 export async function GET(req) {
   const cookieStore = cookies()
