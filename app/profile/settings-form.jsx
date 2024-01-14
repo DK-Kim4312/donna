0b3a9567c86d477e221af2d713fbe1bde33e7cbb
DK Kim <dkim693@wisc.edu>
2024-01-13 23:42:50 -0800

    integration success

--- app/profile/settings-form.jsx
+++ app/profile/settings-form.jsx
@@ -4,7 +4,7 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import '../globals.css';
 import './profile.module.css';
 
-export default function ProfileForm({ session }) {
+export default function SettingsForm({ session }) {
     const supabase = createClientComponentClient();
     const [loadingGoogle, setLoadingGoogle] = useState(false); // Set initial state to false
     const [googleEvents, setGoogleEvents] = useState(null);
@@ -28,24 +28,39 @@ export default function ProfileForm({ session }) {
         }
     }, [session]);
 
-    useEffect(() => {
-        syncGoogleEvents();
-    }, [syncGoogleEvents]);
-
     return (
-        <div className="min-h-screen min-w-screen h-[100%] w-[100%] flex items-center justify-center">
+        <div className="min-h-screen min-w-screen h-[100%] w-[100%] flex flex-col items-center justify-center bg-[#52ab9833]">
             <button
                 onClick={() => syncGoogleEvents()}
                 disabled={loadingGoogle}
+                className='bg-[#52ab98] hover:bg-[#52ab98] text-white font-bold mt-[20px] py-2 px-4 rounded'
             >
                 {loadingGoogle ? 'Loading ...' : 'Sync Google Calendar'}
             </button>
 
             {googleEvents && googleEvents.items && (
-                <ul>
+                <ul className='ml-[10vw] mr-[10vw] overflow-auto max-h-[100vh]'>
                     {googleEvents.items.map((event) => (
-                        <li key={event.id}>{event.summary}</li>
+                        <ul key={event.id} className=' mt-1 ' >
+                            <li>
+                                <span className='font-bold'>Title:</span> {event.summary}
+                            </li>
+                            <li>
+                                <span className='font-bold'>Start:</span> {event.start.dateTime}
+                            </li>
+                            <li>
+                                <span className='font-bold'>End:</span> {event.end.dateTime}
+                            </li>
+                            <li>
+                                <span className='font-bold'>Description:</span> {event.description}
+                            </li>
+                            <li>
+                                <span className='font-bold'>Location:</span> {event.location}
+                            </li>
+
+                        </ul>
                     ))}
+
                 </ul>
             )}
         </div>
