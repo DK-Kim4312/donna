7876d4497c019f9956fc5c71fa27f23a4f455068
DK Kim <dkim693@wisc.edu>
2024-01-28 01:36:42 -0600

    changed navigation

--- components/landing/navigation.jsx
+++ components/landing/navigation.jsx
@@ -12,17 +12,17 @@ export default function Navigation() {
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
-            Features
+            About
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
-            Customers
+            Team
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-            Integrations
+            Pricing
           </Link>
         </NavbarItem>
       </NavbarContent>
