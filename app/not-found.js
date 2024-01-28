f64825bed36a5ab1d1c181f900c7ab16c4a8c5a5
DK Kim <dkim693@wisc.edu>
2024-01-27 23:40:44 -0600

    fonts added to not found page

--- app/not-found.js
+++ app/not-found.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 
 export default function NotFound() {
-  return <div>
-      <h1>Not found – 404!</h1>
+  return ( 
+  <div className='h-[100vh] w-[100vw] flex flex-col items-center justify-center'>
+      <h1 className='text-[#52ab98] text-[80px] font-bold md:text-[72px] sm:text-[50px] xs: text-[32px]'>404</h1>
+        <p className='text-black text-[50px] font-medium md:text-[32px] sm:text-[18px] xs: text-[16px]'>The page that you requested does not exist.</p>
       <div>
-        <Link href="/">Go back to Home</Link>
+        <Link href="/" className='mt-[10px] text-[#52ab98] text-[50px] font-medium md:text-[32px] sm:text-[18px] xs: text-[16px]'>Back to Dashboard</Link>
       </div>
-  </div>
+  </div> );
 }
